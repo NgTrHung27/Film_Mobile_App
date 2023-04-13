404ab0b640100745943eeafbe3668082df1ffbb3
KevinElevenVN <kevineleven9000@gmail.com>
2023-04-13 23:17:07 +0700

    HUNN GAY

--- app/src/main/java/com/example/myapplication/Details_setting_activity/History_Activity.java
+++ app/src/main/java/com/example/myapplication/Details_setting_activity/History_Activity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.Details_setting_activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -16,8 +17,12 @@ import com.example.myapplication.HistoryAdapter;
 import com.example.myapplication.Model.HistoryModel;
 import com.example.myapplication.Model.MovieModel;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -32,7 +37,7 @@ import java.util.List;
 public class History_Activity extends AppCompatActivity  {
 
     FirebaseStorage storage = FirebaseStorage.getInstance();
-    StorageReference storageRef;
+    StorageReference storageRef = FirebaseStorage.getInstance().getReference();
     FirebaseFirestore db;
 
 
@@ -57,9 +62,6 @@ public class History_Activity extends AppCompatActivity  {
 
         storageRef = storage.getReference();
         FirebaseApp.initializeApp(this);
-
-
-
         historyModels = new ArrayList<>();
         historyAdapter = new HistoryAdapter(historyModels);
 
@@ -67,17 +69,35 @@ public class History_Activity extends AppCompatActivity  {
         HistoryWatching.setLayoutManager(new GridLayoutManager(this,3));
         HistoryWatching.setAdapter(historyAdapter);
         db = FirebaseFirestore.getInstance();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        loadHistoryData();
+
         Button btnClearHis = findViewById(R.id.bt_remove_his);
         btnClearHis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                historyModels.clear();
+                db.collection("ViewHistory").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for (QueryDocumentSnapshot documentSnapshot : task.getResult()){
+                            String document = documentSnapshot.getId();
+                            db.collection("ViewHistory").document(document).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                }
+                            });
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
             }
         });
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference filmsRef = db.collection("Film");
-        loadHistoryData();
     }
     private void loadHistoryData(){
         db.collection("ViewHistory").get().addOnCompleteListener(task -> {
